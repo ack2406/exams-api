94ef3725a44c084bbf04bed309471a4f26999943
ack2406 <ack9222@gmail.com>
2022-11-11 15:30:56 +0100

    Add GET requests for questions and answers

--- src/main.py
+++ src/main.py
@@ -50,7 +50,15 @@ def remove_set(set_id: int, db: Session = Depends(get_db)):
     return db_set
 
 
-@app.post("/sets/{set_id}/questions", response_model=schemas.Question)
+
+@app.get("/sets/{set_id}/questions/", response_model=list[schemas.Question])
+def read_questions(set_id: int, db: Session = Depends(get_db)):
+    db_set = crud.read_set(db, set_id=set_id)
+    if db_set is None:
+        raise HTTPException(status_code=404, detail="Set not found")
+    return db_set.questions
+
+@app.post("/sets/{set_id}/questions/", response_model=schemas.Question)
 def create_question(question: schemas.QuestionCreate, set_id: int, db: Session = Depends(get_db)):
     return crud.create_question(db=db, question=question, set_id=set_id)
 
@@ -69,8 +77,14 @@ def remove_question(question_id: int, db: Session = Depends(get_db)):
     return db_question
 
 
+@app.get("/questions/{question_id}/answers/", response_model=list[schemas.Answer])
+def read_answers(question_id: int, db: Session = Depends(get_db)):
+    db_question = crud.read_question(db, question_id=question_id)
+    if db_question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return db_question.answers
 
-@app.post("/questions/{question_id}/answers", response_model=schemas.Answer)
+@app.post("/questions/{question_id}/answers/", response_model=schemas.Answer)
 def create_answer(answer: schemas.AnswerCreate, question_id: int, db: Session = Depends(get_db)):
     return crud.create_answer(db=db, answer=answer, question_id=question_id)
 
