3d2f6789d225a44324837f60989a1453e6ec67b5
ack2406 <ack9222@gmail.com>
2022-11-11 15:28:15 +0100

    Add request handlers

---
+++ src/crud.py
@@ -0,0 +1,58 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+#create_set
+def create_set(db: Session, set: schemas.SetCreate):
+    db_set = models.Set(**set.dict())
+    db.add(db_set)
+    db.commit()
+    db.refresh(db_set)
+    return db_set
+
+#read_set
+def read_set(db: Session, set_id: int):
+    return db.query(models.Set).filter(models.Set.id == set_id).first()
+
+#read_sets
+def read_sets(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Set).offset(skip).limit(limit).all()
+
+#remove_set
+def remove_set(db: Session, set_id: int):
+    db_set = db.query(models.Set).filter(models.Set.id == set_id).first()
+    db.delete(db_set)
+    db.commit()
+    return db_set
+
+
+#create_question
+def create_question(db: Session, question: schemas.QuestionCreate, set_id: int):
+    db_question = models.Question(**question.dict(), set_id=set_id)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    return db_question
+
+#remove_question
+def remove_question(db: Session, question_id: int):
+    db_question = db.query(models.Question).filter(models.Question.id == question_id).first()
+    db.delete(db_question)
+    db.commit()
+    return db_question
+
+
+#create_answer
+def create_answer(db: Session, answer: schemas.AnswerCreate, question_id: int):
+    db_answer = models.Answer(**answer.dict(), question_id=question_id)
+    db.add(db_answer)
+    db.commit()
+    db.refresh(db_answer)
+    return db_answer
+
+#remove_answer
+def remove_answer(db: Session, answer_id: int):
+    db_answer = db.query(models.Answer).filter(models.Answer.id == answer_id).first()
+    db.delete(db_answer)
+    db.commit()
+    return db_answer
--- src/main.py
+++ src/main.py
@@ -1,8 +1,82 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-    
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+@app.get("/sets/", response_model=list[schemas.Set])
+def read_sets(db: Session = Depends(get_db)):
+    sets = crud.read_sets(db)
+    return sets
+
+@app.get("/sets/{set_id}", response_model=schemas.Set)
+def read_set(set_id: int, db: Session = Depends(get_db)):
+    db_set = crud.read_set(db, set_id=set_id)
+    if db_set is None:
+        raise HTTPException(status_code=404, detail="Set not found")
+    return db_set
+
+#add authentication
+@app.post("/sets/", response_model=schemas.Set)
+def create_set(set: schemas.SetCreate, db: Session = Depends(get_db)):
+    return crud.create_set(db=db, set=set)
+
+@app.delete("/sets/{set_id}", response_model=schemas.Set)
+def remove_set(set_id: int, db: Session = Depends(get_db)):
+    db_set = crud.remove_set(db, set_id=set_id)
+    if db_set is None:
+        raise HTTPException(status_code=404, detail="Set not found")
+    return db_set
+
+
+@app.post("/sets/{set_id}/questions", response_model=schemas.Question)
+def create_question(question: schemas.QuestionCreate, set_id: int, db: Session = Depends(get_db)):
+    return crud.create_question(db=db, question=question, set_id=set_id)
+
+@app.get("/questions/{question_id}", response_model=schemas.Question)
+def read_question(question_id: int, db: Session = Depends(get_db)):
+    db_question = crud.read_question(db, question_id=question_id)
+    if db_question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return db_question
+
+@app.delete("/questions/{question_id}", response_model=schemas.Question)
+def remove_question(question_id: int, db: Session = Depends(get_db)):
+    db_question = crud.remove_question(db, question_id=question_id)
+    if db_question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return db_question
+
+
+
+@app.post("/questions/{question_id}/answers", response_model=schemas.Answer)
+def create_answer(answer: schemas.AnswerCreate, question_id: int, db: Session = Depends(get_db)):
+    return crud.create_answer(db=db, answer=answer, question_id=question_id)
+
+@app.delete("/answers/{answer_id}", response_model=schemas.Answer)
+def remove_answer(answer_id: int, db: Session = Depends(get_db)):
+    db_answer = crud.remove_answer(db, answer_id=answer_id)
+    if db_answer is None:
+        raise HTTPException(status_code=404, detail="Answer not found")
+    return db_answer
